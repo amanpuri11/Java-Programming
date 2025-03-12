83c4a266fd5910406f57a0325d2f4b49d195163a
Amanpuri <146429592+amanpuri11@users.noreply.github.com>
2025-03-12 12:23:37 +0530

    Add files via upload

---
+++ for_each.java
@@ -0,0 +1,12 @@
+package loop;
+
+public class for_each {
+	public static void main(String[] args) {
+		int arr[] = {12,23,15,14,19};
+		
+		for(int i : arr) {
+			System.out.println(i);
+	    }
+	}
+
+}
